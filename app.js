9b4177f3d712bb400d8adffd7de417e51c59fcd1
artursem <artur.semeniuk@hmail.com>
2021-04-12 21:23:30 +0200

    js fixes. added axios

--- app.js
+++ app.js
@@ -1,28 +1,52 @@
 // GET CURRENT POSITION
-const currentPosition = {};
 const success = (position) => {
-    //console.log(position.coords.latitude, position.coords.longitude);
-    currentPosition.lat = position.coords.latitude;
-    currentPosition.lon = position.coords.longitude;
+    const currentPosition = {
+        lat: position.coords.latitude,
+        lon:position.coords.longitude
+    };
+    fetchData(currentPosition)
+    .then((response) => {
+        displayResults(response);
+    });
 };
-
-navigator.geolocation.getCurrentPosition(success);
-console.log(currentPosition);
-console.log(currentPosition.lat);
 
 
 // FETCH WEATHER DATA
 const fetchData = async ( position ) => {
     const response = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
         params: {
-            q: 'London',
-            // lat: position.lat,
-            // lon: position.lon,
+            lat: position.lat,
+            lon: position.lon,
             appid: '599f9ab00f5ffd6eeb1a6bf54606a714'
         }
     });
-    console.log(response.data);
+    return response.data;
+};
+
+// DISPLAY RESULTS
+const displayResults = (data) => {
+    console.log(data);
     //update dom
+    const { description, icon } = data.weather[0];
+    const { temp } = data.main;
+
+    const city = document.querySelector('.location-city');
+    const weatherIcon = document.querySelector('#weather-icon');
+    const temperature = document.querySelector('.temperature-degree');
+    const summary = document.querySelector('.temperature-description');
+    const tempBtn = document.querySelector('.temperature');
+    //add F
+    const tempC = Math.round(temp - 273);
+    city.textContent = data.name;
+    weatherIcon.innerHTML = 
+    `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}">`;
+    temperature.textContent = tempC;
+    summary.textContent = description;
+
+
 }
 
-//fetchData(currentPosition);
+// AUTOMATIC
+navigator.geolocation.getCurrentPosition(success);
+
+// CITY INPUT
