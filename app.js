a069c97b0b7b8fd71266d8e655f57b0f17ee9447
artursem <artur.semeniuk@hmail.com>
2021-04-13 19:20:39 +0200

    fixed searchbar

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const success = (position) => {
     };
     fetchData(currentPosition)
     .then((response) => {
-        displayResults(response);
+        showData(response);
     });
+    // add catch
 };
 
 
@@ -24,29 +25,70 @@ const fetchData = async ( position ) => {
 };
 
 // DISPLAY RESULTS
-const displayResults = (data) => {
-    console.log(data);
-    //update dom
+const showData = (data) => {
     const { description, icon } = data.weather[0];
     const { temp } = data.main;
-
     const city = document.querySelector('.location-city');
     const weatherIcon = document.querySelector('#weather-icon');
     const temperature = document.querySelector('.temperature-degree');
     const summary = document.querySelector('.temperature-description');
-    const tempBtn = document.querySelector('.temperature');
-    //add F
-    const tempC = Math.round(temp - 273);
     city.textContent = data.name;
     weatherIcon.innerHTML = 
     `<img src="http://openweathermap.org/img/wn/${icon}@2x.png" alt="${description}">`;
-    temperature.textContent = tempC;
     summary.textContent = description;
-
-
-}
+    
+    const tempBtn = document.querySelector('.temperature');
+    const tempSpan = document.querySelector('.degree-section span');
+    const tempC = Math.round(temp - 273);
+    const tempF = Math.round((tempC * 5 / 9) + 32);
+    temperature.textContent = tempC;
+    tempBtn.addEventListener('click', () => {
+        if(tempSpan.textContent === 'C') {
+            temperature.textContent = tempF;
+            tempSpan.textContent = 'F';
+        } else {
+            temperature.textContent = tempC;
+            tempSpan.textContent = 'C'
+        };
+    });
+};
 
 // AUTOMATIC
 navigator.geolocation.getCurrentPosition(success);
 
 // CITY INPUT
+const onInput = (input) => {
+    const { value } = input.target;
+    if (input != false) {
+    fetchCity(value)
+    .then((response) => {
+        showData(response);
+    });
+    }
+}
+
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return(...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
+
+const fetchCity = async (city) => {
+    const response = await axios.get('http://api.openweathermap.org/data/2.5/weather', {
+        params: {
+            q: city,
+            appid: '599f9ab00f5ffd6eeb1a6bf54606a714'
+        }
+    });
+    return response.data;
+};
+
+
+const input = document.querySelector('input');
+input.addEventListener('input', debounce(onInput));
